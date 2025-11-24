c2d9990553c9271f91d590d13925b67dbb849af7
zahidlizahid777 <zahidlizahid777@gmail.com>
2025-11-24 10:14:38 +0400

    Add files via upload

---
+++ task9.cpp
@@ -0,0 +1,11 @@
+// 9. Armstrong ?d?dl?ri tapmaq (3 r?q?mli)
+void task9() {
+for (int i = 100; i < 1000; i++) {
+int a = i / 100;
+int b = (i / 10) % 10;
+int c = i % 10;
+if (aaa + bbb + ccc == i)
+cout << i << " ";
+}
+cout << endl;
+}
