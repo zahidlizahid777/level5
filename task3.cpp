3c7d1ef8b91dd16426c15e56e9d7dd9c3368e5e4
zahidlizahid777 <zahidlizahid777@gmail.com>
2025-11-24 10:13:30 +0400

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,8 @@
+// 3. Bir ededin kvadratını v? kubunu hesablayan funksiya
+void task3() {
+double a;
+cout << "Eded daxil edin: ";
+cin >> a;
+cout << "Kvadrat: " << a * a << endl;
+cout << "Kub: " << a * a * a << endl;
+}
