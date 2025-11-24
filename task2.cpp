5c5b2f3780e56e0e8e8e66427b1433ff488a857f
zahidlizahid777 <zahidlizahid777@gmail.com>
2025-11-24 10:13:17 +0400

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,10 @@
+// 2. Iki ededin cemini, ferqini, hasilini, bölmesini tapan funksiya
+void task2() {
+double a, b;
+cout << "Iki eded daxil edin: ";
+cin >> a >> b;
+cout << "Cem: " << a + b << endl;
+cout << "Ferq: " << a - b << endl;
+cout << "Hasil: " << a * b << endl;
+cout << "Bolme: " << a / b << endl;
+}
