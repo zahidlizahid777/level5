7170840174865f5445c99d7461b798518e7067ea
zahidlizahid777 <zahidlizahid777@gmail.com>
2025-11-24 10:13:42 +0400

    Add files via upload

---
+++ task4.cpp
@@ -0,0 +1,9 @@
+// 4. Dair?nin sah?si v? çevr?sini tapan funksiya
+void task4() {
+double r;
+const double pi = 3.14159;
+cout << "Radius daxil edin: ";
+cin >> r;
+cout << "Sahe: " << pi * r * r << endl;
+cout << "Cevre: " << 2 * pi * r << endl;
+}
